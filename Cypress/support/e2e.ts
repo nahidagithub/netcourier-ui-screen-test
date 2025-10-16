33fb04bc5330e5d363fee0ad02d7e763d364ba16
nahidagithub <148271590+nahidagithub@users.noreply.github.com>
2025-10-16 13:40:25 +0600

    Update e2e.ts

--- Cypress/support/e2e.ts
+++ Cypress/support/e2e.ts
@@ -1,1 +1,21 @@
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax
+// /// <reference path="./auth-commands.d.ts" />
+import './commands';
+import './auth-commands';
+import 'cypress-mochawesome-reporter/register';
 
