95990dff4ba03e60401c05975a70be089054f58f
nahidagithub <148271590+nahidagithub@users.noreply.github.com>
2025-10-16 13:31:42 +0600

    Update authcommand.d.ts

--- Cypress/support/authcommand.d.ts
+++ Cypress/support/authcommand.d.ts
@@ -1,1 +1,13 @@
+/// <reference types="cypress" />
+export {}; // Ensures this file is treated as a module
+
+declare global {
+	namespace Cypress {
+		interface Chainable<Subject = any> {
+			login(): Chainable<void>;
+			visitAndSyncCookies(url: string): Chainable<void>;
+			handleEscalateRole(password: string): Chainable<void>;
+		}
+	}
+}
 
